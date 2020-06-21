c02eeb283eaaa631423198962ec72a0dcb35a0ab
G Mahesh <48273824+Maheshoo7@users.noreply.github.com>
2020-06-21 23:45:34 +0530

    preparing to deploy on heroku

--- Myproject/settings.py
+++ Myproject/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import dj_database_url
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -26,7 +25,9 @@ SECRET_KEY = 'y(op_j*pl8nk!_^t3sigvtujwg*s%awz+)+qkr5-#d5_ww)zua'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['useractivityapi.herokuapp.com']
+# ALLOWED_HOSTS = ['useractivityapi.herokuapp.com']
+ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -136,3 +137,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
